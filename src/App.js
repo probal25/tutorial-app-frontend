9d0bdd43c9ced36add68ed837b0c1b20c7da87d9
probal25 <probal@divine-it.net>
2021-11-28 00:35:22 +0600

    Add tutorial feature added to Frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ListTutorialComponents from './components/ListTutorialComponents';
-import HeaderComponents from './components/HeaderComponents';
-import FooterComponents from './components/FooterComponents';
+import ListTutorialComponents from './components/TutorialComponents/ListTutorialComponents';
+import HeaderComponents from './components/Common/HeaderComponents';
+import FooterComponents from './components/Common/FooterComponents';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import CreateTutorialComponents from './components/CreateTutorialComponents';
+import CreateTutorialComponents from './components/TutorialComponents/CreateTutorialComponents';
 
 function App() {
   return (
